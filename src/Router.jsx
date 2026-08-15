10ca042c78d9c7a294199e9ed002d0eb9d65647d
alfan <alfannurfaizwork@gmail.com>
2026-08-15 15:29:43 -0500

    feat: setting initial router

---
+++ src/Router.jsx
@@ -0,0 +1,34 @@
+import { Routes, Route, Navigate } from "react-router";
+
+import Home from "./pages/Home.jsx";
+import Events from "./pages/Events.jsx";
+import Communities from "./pages/Communities.jsx";
+import EventDetail from "./pages/EventDetail.jsx";
+import CommunityDetail from "./pages/CommunityDetail.jsx";
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/explore" replace />} />
+      <Route path="/explore" element={<Home />} />
+
+      <Route path="/events">
+        <Route index element={<Events />} />
+        <Route path=":category/:location" element={<Events />} />
+        <Route path=":id" element={<EventDetail />} />
+      </Route>
+
+      <Route path="/communities">
+        <Route index element={<Navigate to="/communities/all" replace />} />
+        <Route path="all" element={<Communities />} />
+        <Route path=":search" element={<Communities />} />
+      </Route>
+
+      <Route path="/communities">
+        <Route index path=":id" element={<CommunityDetail />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default Router;
