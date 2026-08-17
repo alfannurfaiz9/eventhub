bc0349642f715726fef4ca00849ebfbb1e34d931
alfan <alfannurfaizwork@gmail.com>
2026-08-17 18:29:22 -0500

    fix: fix bug validation auth

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,15 +7,18 @@ import {
 import { IoIosNotificationsOutline } from "react-icons/io";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoMoonOutline } from "react-icons/io5";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BiHomeAlt } from "react-icons/bi";
 import { RiGroupLine } from "react-icons/ri";
 import { PiSignOutBold } from "react-icons/pi";
 import { AiOutlineClose } from "react-icons/ai";
 
+import { getUser } from "../utils/getDatas.js";
+
 const Navbar = () => {
   const [showPopUp, setShowPopUp] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
+  const [user, setUser] = useState(null);
 
   const logedInUser = localStorage.getItem("isLogin");
 
@@ -26,6 +29,12 @@ const Navbar = () => {
 
     navigate("/login");
   };
+
+  useEffect(() => {
+    (() => {
+      setUser(getUser());
+    })();
+  }, []);
 
   return (
     <header className="text-sm py-4 lg:py-3 px-6 flex gap-4 items-center shadow-sm sticky top-0 z-50 bg-white">
@@ -72,7 +81,7 @@ const Navbar = () => {
             </li>
             <li className={logedInUser ? "block" : "hidden"}>
               <NavLink
-                to="/communities"
+                to="/my-events"
                 className={({ isActive }) =>
                   `px-3 py-1.5 rounded-md ${isActive ? "bg-light-primary text-primary" : "text-black"}`
                 }
@@ -109,12 +118,12 @@ const Navbar = () => {
         >
           <div className={logedInUser ? "flex items-center gap-4" : "hidden"}>
             <div className="flex items-center">
-              <button className="cursor-pointer relative">
+              <Link to="/notifications" className="cursor-pointer relative">
                 <div className="absolute -top-0.5 -right-0.5 h-3.5 w-3.5 z-10 flex items-center justify-center rounded-full bg-primary text-white">
-                  <p className="text-[7px]">3</p>
+                  <p className="text-[7px]">1</p>
                 </div>
                 <IoIosNotificationsOutline className="text-2xl" />
-              </button>
+              </Link>
             </div>
             <IoMoonOutline className="text-xl cursor-pointer" />
           </div>
@@ -125,17 +134,17 @@ const Navbar = () => {
             >
               <img
                 className="w-6 h-6 lg:w-7 lg:h-7 rounded-full"
-                src="https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250"
+                src={user?.img}
                 alt="profile-pict"
               />
               <div
                 className={`${showPopUp ? "grid" : "hidden"} bg-white text-xs shadow-2xl border border-gray-300 absolute top-11 right-0 text-start gap-2 p-2 rounded-lg`}
               >
                 <div className="border-b border-b-gray-300 p-2">
-                  <p className="font-bold">Alex kim</p>
-                  <p className="text-dark-gray">alex.kim@example.com</p>
+                  <p className="font-bold">{user?.full_name}</p>
+                  <p className="text-dark-gray">{user?.email}</p>
                 </div>
-                <Link className="border-b border-b-gray-300 p-2">
+                <Link to="/profile" className="border-b border-b-gray-300 p-2">
                   My profile
                 </Link>
                 <p
@@ -151,12 +160,12 @@ const Navbar = () => {
         <div className="relative lg:hidden flex items-center gap-4">
           <div className="lg:hidden flex items-center gap-4">
             <div className={logedInUser ? "flex items-center" : "hidden"}>
-              <button className="cursor-pointer relative">
+              <Link to="/notifications" className="cursor-pointer relative">
                 <div className="absolute -top-0.5 -right-0.5 h-3.5 w-3.5 z-10 flex items-center justify-center rounded-full bg-primary text-white">
                   <p className="text-[7px]">3</p>
                 </div>
                 <IoIosNotificationsOutline className="text-2xl" />
-              </button>
+              </Link>
             </div>
             <IoMoonOutline className="text-xl cursor-pointer" />
           </div>
@@ -173,19 +182,19 @@ const Navbar = () => {
             className={`${showMenu ? "block" : "hidden"} text-xl`}
           />
           <div
-            className={`${showMenu ? "block" : "hidden"} absolute bg-white top-9 right-0 min-w-50 rounded-lg shadow-sm border border-gray-300`}
+            className={`${showMenu ? "block" : "hidden"} absolute bg-white top-9 right-0 min-w-60 rounded-lg shadow-sm border border-gray-300`}
           >
             <div
               className={logedInUser ? "p-2 flex items-center gap-2" : "hidden"}
             >
               <img
                 className="w-7 h-7 lg:w-7 lg:h-7 rounded-full"
-                src="https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250"
+                src={user?.img}
                 alt="profile-pict"
               />
               <div>
-                <p className="font-semibold text-sm">Alex Kim</p>
-                <p className="text-dark-gray text-xs">alexkim@example.com</p>
+                <p className="font-semibold text-sm">{user?.full_name}</p>
+                <p className="text-dark-gray text-xs">{user?.email}</p>
               </div>
             </div>
             <ul>
@@ -224,7 +233,7 @@ const Navbar = () => {
               </li>
               <li className={logedInUser ? "w-full" : "hidden"}>
                 <NavLink
-                  to="/my-event"
+                  to="/my-events"
                   className={({ isActive }) =>
                     `p-3 w-full flex items-center gap-2 ${isActive ? "bg-light-primary text-primary" : "text-black"}`
                   }
