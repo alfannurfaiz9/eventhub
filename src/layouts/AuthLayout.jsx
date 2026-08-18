c0931806a78794ca9c88972f85f222064720d70f
alfan <alfannurfaizwork@gmail.com>
2026-08-18 05:46:47 -0500

    refactor: remove unused imported variable

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,7 +1,18 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import Discover from "../components/Discover";
+import { getUser } from "../utils/getDatas";
+import { useEffect } from "react";
 
 const AuthLayout = () => {
+  const logedInUser = localStorage.getItem("isLogin");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (logedInUser) {
+      navigate("/");
+      return;
+    }
+  }, [logedInUser, navigate]);
   return (
     <>
       <section className="flex lg:justify-between min-h-dvh">
